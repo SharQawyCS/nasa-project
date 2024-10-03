535ae5cfc73ab6547b738fa23d3d7c28a18c8de8
Marwan-Ah <23P0242@eng.asu.edu.eg>
2024-10-03 22:14:31 +0300

    Login and Sign up disappears after logging in

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,20 +4,36 @@ import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import './Header.css'; // Import the custom CSS
 
 const Header = () => {
-    return (
-        <header className="header">
-            <div className="header-logo">NASA Space App</div>
-            <nav className="header-nav">
-                {/* Use Link instead of a for client-side navigation */}
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/signup">Signup</Link>
-                {/* <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link> */}
-            </nav>
-        </header>
-    );
+    let loggedin = localStorage.getItem('isLoggedIn');
+    if(loggedin === "true"){
+        return (
+            <header className="header">
+                <div className="header-logo">NASA Space App</div>
+                <nav className="header-nav">
+                    {/* Use Link instead of a for client-side navigation */}
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                    {/* <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact</Link> */}
+                </nav>
+            </header>
+        );
+    } else {
+        return (
+            <header className="header">
+                <div className="header-logo">NASA Space App</div>
+                <nav className="header-nav">
+                    {/* Use Link instead of a for client-side navigation */}
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/login">Login</Link>
+                    <Link to="/signup">Signup</Link>
+                    {/* <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact</Link> */}
+                </nav>
+            </header>
+        );
+    }
 };
 
 export default Header;
