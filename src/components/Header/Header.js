054a6fc5515c67d227bf9484a56df75e1c062679
Marwan-Ah <23P0242@eng.asu.edu.eg>
2024-10-03 22:45:03 +0300

    login disappears and reappears

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,10 @@ import React from 'react';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import './Header.css'; // Import the custom CSS
 
+const logout = () => {
+    localStorage.setItem('isLoggedIn','false');
+}
+
 const Header = () => {
     let loggedin = localStorage.getItem('isLoggedIn');
     if(loggedin === "true"){
@@ -13,6 +17,7 @@ const Header = () => {
                     {/* Use Link instead of a for client-side navigation */}
                     <Link to="/">Home</Link>
                     <Link to="/about">About</Link>
+                    <Link onClick={logout} to="/">Log out</Link>
                     {/* <Link to="/projects">Projects</Link>
                     <Link to="/contact">Contact</Link> */}
                 </nav>
